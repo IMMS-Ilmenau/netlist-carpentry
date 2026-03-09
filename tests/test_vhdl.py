cbd207982cea1c749822c75e9c06d977a214fdd1
Manuel Jirsak <manuel.jirsak@imms.de>
2026-03-09 11:47:49 +0000

    Marked failing test in public repo as xfail since issue stems from OSS CAD SUITE

--- tests/test_vhdl.py
+++ tests/test_vhdl.py
@@ -7,15 +7,18 @@ from netlist_carpentry import read
 
 def test_vhdl_simple_counter() -> None:
     source_path = 'oss-cad-suite/environment' if os.environ.get('CI_SKIP_EQY') == 'true' else '~/oss_cad_suite/environment'
-    c = read('tests/files/vhdl/simple_counter.vhdl', source_paths=[source_path])
-    assert len(c) == 1
-    assert c.has_top
-    assert '§buf' in c.instances
-    assert '§adff' in c.instances
-    assert '§add' in c.instances
-    assert len(c.instances['§buf']) == 16
-    assert len(c.instances['§adff']) == 1
-    assert len(c.instances['§add']) == 1
+    try:
+        c = read('tests/files/vhdl/simple_counter.vhdl', source_paths=[source_path])
+        assert len(c) == 1
+        assert c.has_top
+        assert '§buf' in c.instances
+        assert '§adff' in c.instances
+        assert '§add' in c.instances
+        assert len(c.instances['§buf']) == 16
+        assert len(c.instances['§adff']) == 1
+        assert len(c.instances['§add']) == 1
+    except FileNotFoundError:
+        pytest.xfail('Installation of OSS CAD SUITE failed!')
 
 
 if __name__ == '__main__':
